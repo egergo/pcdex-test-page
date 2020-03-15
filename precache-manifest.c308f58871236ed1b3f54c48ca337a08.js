e8273b3134ae263e68431e31dca05cbbe3ad242e
Gergo Ertli <egergo@fb.com>
2020-03-16 01:21:56 +0200

    Updates

---
+++ precache-manifest.c308f58871236ed1b3f54c48ca337a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d717b26ce62cf5df4611075d35c95abb",
+    "url": "/pcdex-test-page/index.html"
+  },
+  {
+    "revision": "c2112b6c66a49b53e19f",
+    "url": "/pcdex-test-page/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c76016bb9ab46e9740df",
+    "url": "/pcdex-test-page/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "c2112b6c66a49b53e19f",
+    "url": "/pcdex-test-page/static/js/2.a0f2fe73.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/pcdex-test-page/static/js/2.a0f2fe73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76016bb9ab46e9740df",
+    "url": "/pcdex-test-page/static/js/main.1a28b563.chunk.js"
+  },
+  {
+    "revision": "20e41a5944dd7c086a32",
+    "url": "/pcdex-test-page/static/js/runtime-main.69a97313.js"
+  }
+]);
